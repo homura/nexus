6c13cf0660b209e5106ef7fe030fb6735fa159db
homura <homura.dev@gmail.com>
2023-03-18 00:11:12 +0800

    chore: verbose build info
    Signed-off-by: homura <homura.dev@gmail.com>

--- scripts/build-extension.ts
+++ scripts/build-extension.ts
@@ -1,14 +1,34 @@
 import { Auto, IPlugin } from '@auto-it/core';
-import { execSync } from 'child_process';
+import { spawn } from 'child_process';
 
 export default class BuildExtension implements IPlugin {
   name = 'nexus-build-extension';
 
   apply(auto: Auto): void {
     auto.hooks.afterVersion.tapPromise(this.name, async () => {
-      execSync('npm run build:libs');
-      execSync('npm run build:extension-chrome');
-      execSync('npm run -w @nexus-wallet/extension-chrome zip');
+      await this.buildNexusExtension(auto);
+    });
+    auto.hooks.canary.tapPromise(this.name, async () => {
+      await this.buildNexusExtension(auto);
+    });
+  }
+
+  private buildNexusExtension(auto: Auto): Promise<void> {
+    return new Promise((resolve) => {
+      const build = spawn('npm', ['run', 'build:extension-chrome']);
+
+      build.stdout.on('data', (data) => {
+        auto.logger.verbose.info(data.toString());
+      });
+
+      build.stderr.on('data', (data) => {
+        auto.logger.verbose.info(data.toString());
+      });
+
+      build.on('close', () => {
+        auto.logger.log.info('Nexus extension built successfully.');
+        resolve();
+      });
     });
   }
 }
